56c3540c283a2edeffad88bd6d7bedeba80890ee
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-10-10 00:14:31 -0700

    test
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- tests/test.rs
+++ tests/test.rs
@@ -1,1 +1,17 @@
+extern crate failure;
+extern crate html_index;
 
+use failure::Error;
+
+#[test]
+fn should_work() -> Result<(), Error> {
+  let res = html_index::Builder::new()
+    .raw_body("<body>hello world</body>")
+    .script("/bundle.js")
+    .style("/bundle.css")
+    .build();
+
+  let expected = include_str!("./fixtures/html-css.html");
+  assert_eq!(res, expected);
+  Ok(())
+}
